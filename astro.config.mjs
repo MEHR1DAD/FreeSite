715af0fc8da7c134cab4630612e6e2a8c58ce997
Mehrdad Oliaee <mehr1dado@gmail.com>
2026-08-27 21:24:38 +0330

    feat(deploy): integrate @astrojs/cloudflare adapter for zero-config Pages builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://freesite.pages.dev',
   output: 'static',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
